c91954ee8263c9ae6d0df05e538796f916bd93d1
marrabl <162188192+marrabl@users.noreply.github.com>
2024-03-06 21:39:53 +0100

    Variablen Deklaration, Button Binding

--- app/src/main/java/com/example/se2_einzelbeispiel_rablbauer/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelbeispiel_rablbauer/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.se2_einzelbeispiel_rablbauer;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText matNrInput;
+    TextView serverResponse;
+    Socket socket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        matNrInput = findViewById(R.id.inputMatrikelnummer);
+        serverResponse = findViewById(R.id.textViewSever);
+
+        Button btnAbschicken = findViewById(R.id.buttonAbschicken);
+        btnAbschicken.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
